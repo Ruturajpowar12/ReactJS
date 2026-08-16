83a856f2ba4b558eccc8d54d60f71f696cc51b4e
Ruturaj Powar <ruturajpowar2038@gmail.com>
2026-08-16 13:58:20 +0530

    understand the react flow and structure

--- 01-viteReact/src/App.jsx
+++ 01-viteReact/src/App.jsx
@@ -1,9 +1,14 @@
+import Chai from "./Chai.jsx";
+
 function App() {
   return (
-    <div>
+    <>
       <h1>Ruturaj Powar</h1>
+      <p>
+        <Chai /> is ready
+      </p>
       <p>basic react for vite bundle install and setup </p>
-    </div>
+    </>
   );
 }
 
