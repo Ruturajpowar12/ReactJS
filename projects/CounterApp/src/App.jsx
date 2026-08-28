e5d28d3e238343969903eff76d872e6ca74d1cb9
Ruturaj Powar <ruturajpowar2038@gmail.com>
2026-08-28 08:17:39 +0530

    counter functionality with increase, reset, and decrease options

--- projects/CounterApp/src/App.jsx
+++ projects/CounterApp/src/App.jsx
@@ -1,7 +1,54 @@
-import React from "react";
+import { useState } from "react";
 
 function App() {
-  return <div>App</div>;
+  const [count, setCount] = useState(0);
+
+  function increaseCount() {
+    setCount((prev) => prev + 1);
+  }
+  function restCount() {
+    setCount(0);
+  }
+  function decreaseCount() {
+    setCount((prev) => prev - 1);
+  }
+
+  return (
+    <div
+      className="w-full h-screen  flex 
+      justify-center
+      items-center flex-col gap-5 bg-linear-to-bl to-blue-400 from-green-400"
+    >
+      <h1 className="text-blue-800 text-6xl font-bold">Counter App</h1>
+      <div className="flex justify-center items-center flex-col py-10 rounded-xl shadow-2xl w-140  gap-3 bg-white">
+        <div className="top">
+          <h2 className=" font-extrabold text-8xl text-blue-800">{count}</h2>
+        </div>
+        <div className="bottom w-full flex justify-around px-10 mt-10">
+          <button
+            className="p-4 bg-blue-900 text-white font-semibold rounded-2xl shadow-lg hover:bg-blue-800"
+            onClick={increaseCount}
+          >
+            Increase
+          </button>
+          <button
+            className="p-4 bg-blue-900 text-white font-semibold rounded-2xl shadow-lg hover:bg-blue-800"
+            onClick={restCount}
+          >
+            Reset
+          </button>
+          <button
+            className="p-4 bg-blue-900 text-white font-semibold rounded-2xl shadow-lg hover:bg-blue-800"
+            onClick={decreaseCount}
+            disabled={count <= 0}
+          >
+            Decrease
+          </button>
+        </div>
+      </div>
+      <h3 className="font-semibold">Created by Ruturaj</h3>
+    </div>
+  );
 }
 
 export default App;
