15f1e9df354821fadd6b2e67ee798ed6b8dbd4bd
Ruturajpowar12 <ruturajpowar2038@gmail.com>
2026-09-21 14:47:49 +0530

    authentication and authorization components and megaBlog is done

---
+++ 12MegaBlog/src/appwrite/auth.js
@@ -0,0 +1,57 @@
+import conf from "../conf/conf.js";
+import { Client, Account, ID } from "appwrite";
+
+export class AuthService {
+  client = new Client();
+  account;
+
+  constructor() {
+    this.client
+      .setEndpoint(conf.appwriteUrl)
+      .setProject(conf.appwriteProjectId);
+    this.account = new Account(this.client);
+  }
+
+  async createAccount({ email, password, name }) {
+    const userAccount = await this.account.create({
+      userId: ID.unique(),
+      email,
+      password,
+      name,
+    });
+    if (userAccount) {
+      // call another method
+      return this.login({ email, password });
+    } else {
+      return userAccount;
+    }
+  }
+
+  async login({ email, password }) {
+    return await this.account.createSession({ email, password });
+  }
+
+  async getCurrentUser() {
+    try {
+      return await this.account.get();
+    } catch (error) {
+      if (error.code !== 401) {
+        console.error("Appwrite service :: getCurrentUser :: error", error);
+      }
+    }
+
+    return null;
+  }
+
+  async logout() {
+    try {
+      await this.account.deleteSessions();
+    } catch (error) {
+      console.log("Appwrite serive :: logout :: error", error);
+    }
+  }
+}
+
+const authService = new AuthService();
+
+export default authService;
